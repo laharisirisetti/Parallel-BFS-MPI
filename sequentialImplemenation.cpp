7b3800f7b5b7aeac080213a6c9de910b640a9d6d
Lahari Sirisetti <laharisirisetti@gmail.com>
2026-08-09 11:08:38 +0530

    changed sequential algorithm to match parallel intput and out put

--- sequentialImplemenation.cpp
+++ sequentialImplemenation.cpp
@@ -1,58 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-     int V,E;
-     cin>>V>>E;
-     vector<vector<int>> edges(E, vector<int> (3));
-     for(int i=0;i<E;i++){
-          cin>>edges[i][0]>>edges[i][1]>>edges[i][2];
-     }
-     int k;
-     cin>>k;
-     vector<int> explorers(k);
-     for(int i=0;i<k;i++){
-          cin>>explorers[i];
-     }
-     int R;
-     cin>>R;
-     int L;
-     cin>>L;
-     vector<bool> isBlockedChamber(V);
-     for(int i=0;i<L;i++){
-          int node;
-          cin>>node;
-          isBlockedChamber[node] = true;
-     }
+vector<int> runBFS(const vector<vector<int>>& graph, int source)
+{
+    int V = graph.size();
 
-     vector<vector<int>> graph(V);
-     for(int i=0;i<E;i++){
-          int u = edges[i][0], v = edges[i][1], d = edges[i][2];
-          graph[u].push_back(v);
-          if(d) graph[v].push_back(u);
-     }
+    vector<int> dist(V, -1);
+    queue<int> q;
 
-     vector<int> dist(V,-1);
-     vector<bool> visited(V, false);
-     queue<int> q;
-     dist[R] = 0;
-     visited[R] = true;
-     q.push(R);
+    dist[source] = 0;
+    q.push(source);
 
-     while(!q.empty()){
-          int node = q.front();
-          q.pop();
-          for(int i=0;i<graph[node].size();i++){
-               int child = graph[node][i];
-               if(!visited[child] && !isBlockedChamber[child]){
-                    visited[child] = true;
-                    dist[child] = dist[node] + 1;
-                    q.push(child);
-               }
-          }
-     }
-     for(int i=0;i<k;i++){
-          cout<<dist[explorers[i]]<<" ";
-     }
-     cout<<endl;
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+
+        for (int child : graph[node])
+        {
+            if (dist[child] == -1)
+            {
+                dist[child] = dist[node] + 1;
+                q.push(child);
+            }
+        }
+    }
+
+    return dist;
+}
+
+void readInputAndBuildGraph(
+    int& V,
+    int& source,
+    vector<vector<int>>& graph)
+{
+    int E;
+    cin >> V >> E >> source;
+
+    graph.resize(V);
+
+    for (int i = 0; i < E; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        graph[u].push_back(v);
+    }
+}
+
+void printShortestPaths(const vector<int>& dist)
+{
+    cout << "shortest paths" << endl;
+
+    for (int d : dist)
+    {
+        cout << d << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    int V;
+    int source;
+    vector<vector<int>> graph;
+
+    readInputAndBuildGraph(V, source, graph);
+
+    vector<int> dist = runBFS(graph, source);
+
+    printShortestPaths(dist);
+
+    return 0;
 }
